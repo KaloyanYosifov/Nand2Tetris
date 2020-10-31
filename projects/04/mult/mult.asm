5d4f1ed3e3a1e510992de75efeb45f86e0f6591d
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-10-31 17:15:08 +0200

    Integrate multiplicator

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@0
+D=A
+@i
+M=D
+
+// init r2 memory
+@0
+D=A
+@R2
+M=D
+
+(MULT_LOOP)
+@R1
+D=M
+@i
+D=D-M
+@END
+D;JEQ
+
+// add the number
+@R0
+D=M
+@R2
+M=D+M
+
+@1
+D=A
+@i
+M=M+D
+
+@MULT_LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
