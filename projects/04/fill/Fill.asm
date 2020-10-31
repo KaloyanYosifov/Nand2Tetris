a391f13cc7b8f46280f8f169683dea009d542908
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-10-31 18:28:56 +0200

    Integrate fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+@SCREEN
+D=A
+@currentScreenPointer
+M=D
+
+@KBD
+D=M
+@FILL_BLACK
+D;JNE
+
+@FILL_WHITE
+0;JMP
+
+(FILL_WHITE)
+@currentScreenPointer
+D=M
+@KBD
+D=A-D
+@LOOP
+D;JEQ
+
+// set the current address pixels
+@0
+D=A
+@currentScreenPointer
+A=M
+M=D
+
+@1
+D=A
+@currentScreenPointer
+M=M+D
+@FILL_WHITE
+0;JMP
+
+(FILL_BLACK)
+@currentScreenPointer
+D=M
+@KBD
+D=A-D
+@LOOP
+D;JEQ
+
+// set the current address pixels
+@0
+D=A-1
+@currentScreenPointer
+A=M
+M=D
+
+@1
+D=A
+@currentScreenPointer
+M=M+D
+@FILL_BLACK
+0;JMP
